80ee9d7032c2f3943d12d39d883b014571676d87
RidwanMuzaki <muzaki2048@gmail.com>
2022-06-12 15:20:10 +0700

    Routing Practicum 3

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,30 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 
 Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+//Practicum 3
+/* Route::redirect('/', 'https://www.educastudio.com');
+
+Route::prefix('category')->group(function () {
+
+    Route::redirect('/marble-edu-games', 'https://www.educastudio.com/category/marbel-edu-games');
+
+    Route::redirect('/marble-and-friends-kids-games', 'https://www.educastudio.com/category/marbel-and-friends-kids-games');
+
+    Route::redirect('/riri-story-books', 'https://www.educastudio.com/category/riri-story-books');
+
+    Route::redirect('/kolak-kids-songs', 'https://www.educastudio.com/category/kolak-kids-songs');
+});
+
+Route::redirect('/news', 'https://www.educastudio.com/news');
+
+Route::prefix('program')->group(function () {
+
+    Route::redirect('/karir', 'https://www.educastudio.com/program/karir');
+
+    Route::redirect('/magang', 'https://www.educastudio.com/program/magang');
+
+    Route::redirect('/kunjungan-industri', 'https://www.educastudio.com/program/kunjungan-industri');
+});
+
+Route::redirect('/about-us', 'https://www.educastudio.com/about-us'); */
