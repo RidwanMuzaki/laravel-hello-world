7d58e4fc361fa5db413d042583301c836cefb119
RidwanMuzaki <muzaki2048@gmail.com>
2022-06-12 14:04:09 +0700

    Routing Practicum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Practicum 1
-Route::get('/', function () {
+//Routing Practicum 1
+/* Route::get('/', function () {
     echo 'Hi! Welcome To Laravel';
 });
 
@@ -22,4 +25,11 @@ Route::view('/welcome', 'welcome');
 
 Route::get('/articles/{id}', function($id) {
     echo "This is Article Pages with ID: ".$id;
-}); 
+});*/
+
+//Routing Practicum 2
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'about']);
+
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
